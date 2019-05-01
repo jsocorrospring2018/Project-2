5f8d16c95e9ac382032fcc5b284013b10f7b25a3
Jorell <jsocorro@hawk.iit.edu>
2019-05-01 14:29:23 -0500

    Added script but had remove formspree :(

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function validateForm()  {
+  var x = document.forms["myForm"]["email"].value;
+  var y = document.forms["myForm"]["message"].value;
+
+  if(x == ""){
+    alert("Email and comment must be filled out");
+    return false;
+  }
+
+  if(y == ""){
+    alert("Email and comment must be filled out");
+    return false;
+  }
+}\
